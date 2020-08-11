76141d8193425810116116125a192e42c02efe6a
Ankush Chauhan <67872399+An-prog-hub@users.noreply.github.com>
2020-08-11 21:03:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+// Cat Generator 
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
